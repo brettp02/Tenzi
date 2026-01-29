9dd652bfbd312311054775597d8e3c00d97d3a3c
Brett <brettpenwarden@gmail.com>
2026-01-30 11:46:33 +1300

    Added dark mode

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,10 @@
 import { ComponentExample } from "@/components/component-example";
+import { AnimatedThemeToggler } from "@/components/ui/animated-theme-toggler"
 
 export default function Page() {
     return (
-        <></>
+        <div>
+            <AnimatedThemeToggler duration={600}/>
+        </div>
     )
 }
