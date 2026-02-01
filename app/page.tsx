ac83057cc573762293d920d3ba3dac4aaaee8e98
Brett <brettpenwarden@gmail.com>
2026-02-02 12:48:23 +1300

    added functionality to hold die

--- app/page.tsx
+++ app/page.tsx
@@ -2,31 +2,63 @@
 
 import Die from "@/components/Die";
 import {useState} from "react";
+import {Button} from "@/components/ui/button";
+import {nanoid} from "nanoid";
 
 export default function Page() {
+    const [dice, setDice] = useState(generateAllNewDice())
+
     function generateAllNewDice() {
         const newArray = []
 
         for (let i = 0; i < 10; i++) {
             const randomNum = Math.floor(Math.random() * 6) + 1
-            newArray.push(randomNum)
+            newArray.push({
+                value: randomNum,
+                isHeld: false,
+                id: nanoid()
+            })
         }
 
         return newArray
     }
-    const [dieNumbers, setDieNumbers] = useState(generateAllNewDice())
 
+    function hold(id: string) {
+        setDice(previousDice => {
+            return previousDice.map(die => {
+                return die.id === id ?
+                    {...die, isHeld: !die.isHeld}:
+                    die
+            })
+        })
+    }
 
+    function rollDice() {
+        setDice(previousDice => {
+            return previousDice.map(die => {
+                return die.isHeld ?
+                    die:
+                    {...die, value: Math.floor(Math.random() * 6) + 1}
 
-    generateAllNewDice()
+            })
+        })
+    }
+
     return (
         <main className={'flex items-center justify-center h-[100vh]'}>
-            <div className={'flex flex-col items-center justify-center bg-muted w-[75vw] h-[75vh] max-w-[700px] rounded-xl overflow-hidden'}>
+            <div className={'flex flex-col items-center justify-evenly bg-muted w-[75vw] h-[75vh] max-w-[700px] rounded-xl overflow-hidden'}>
+                <h1>Title</h1>
+
                 <div className={'grid grid-cols-5 gap-3'}>
-                    {dieNumbers.map((item, i) => {
-                        return <Die value={item} key={i}/>
+                    {dice.map((die, i) => {
+                        return <Die value={die.value} key={die.id} isHeld={die.isHeld} hold={() => hold(die.id)}/>
                     })}
                 </div>
+
+                <Button className={'cursor-pointer w-[100] h-[60] text-xl font-semibold'}
+                onClick={rollDice}>
+                    Roll
+                </Button>
             </div>
         </main>
     )
--- components/Die.tsx
+++ components/Die.tsx
@@ -1,9 +1,16 @@
+import {cn} from "@/lib/utils"
 import {Button} from "@/components/ui/button";
 
-export default function Die({value} : {value: number}) {
+export default function Die({value, isHeld, hold} : {value: number, isHeld: boolean, hold: () => void}) {
     return (
-        <Button className={"bg-white w-[75] h-[75] flex items-center justify-center rounded-md shadow-md cursor-pointer"}>
+        <Button className={`bg-white w-[75] h-[75] flex items-center justify-center rounded-md shadow-md cursor-pointer
+                            ${isHeld ? "bg-[#59E391]" : "bg-white"}`}
+                onClick={hold}
+        >
+
             <p className={'text-secondary text-3xl font-bold'}>{value}</p>
+
         </Button>
     )
+
 }
