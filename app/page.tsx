1333a888cce55963577200bc761530402a58076c
Brett <brettpenwarden@gmail.com>
2026-02-02 13:20:50 +1300

    added win game logic and restart

--- app/page.tsx
+++ app/page.tsx
@@ -4,9 +4,13 @@ import Die from "@/components/Die";
 import {useState} from "react";
 import {Button} from "@/components/ui/button";
 import {nanoid} from "nanoid";
+import Confetti from 'react-confetti'
 
 export default function Page() {
-    const [dice, setDice] = useState(generateAllNewDice())
+    const [dice, setDice] = useState(() => generateAllNewDice())
+
+    const gameWon = (dice.every(die => die.isHeld) &&
+        dice.every(die => die.value === dice[0].value))
 
     function generateAllNewDice() {
         const newArray = []
@@ -34,20 +38,23 @@ export default function Page() {
     }
 
     function rollDice() {
-        setDice(previousDice => {
-            return previousDice.map(die => {
-                return die.isHeld ?
-                    die:
-                    {...die, value: Math.floor(Math.random() * 6) + 1}
+       gameWon ? setDice(() => generateAllNewDice()) :
+           setDice(previousDice => {
+               return previousDice.map(die => {
+                   return die.isHeld ?
+                       die:
+                       {...die, value: Math.floor(Math.random() * 6) + 1}
 
-            })
-        })
+               })
+           })
     }
 
     return (
         <main className={'flex items-center justify-center h-[100vh]'}>
-            <div className={'flex flex-col items-center justify-evenly bg-muted w-[75vw] h-[75vh] max-w-[700px] rounded-xl overflow-hidden'}>
-                <h1>Title</h1>
+            {gameWon && <Confetti />}
+            <div className={'flex flex-col items-center justify-evenly bg-muted/75 w-[75vw] h-[75vh] max-w-[700px] rounded-xl overflow-hidden container mx-auto px-12'}>
+                <h1 className="font-bold text-5xl">Tenzi</h1>
+                <p className="text-muted-foreground text-center text-lg">Roll until all dice are the same. Click each die to freeze it at its current value between rolls.</p>
 
                 <div className={'grid grid-cols-5 gap-3'}>
                     {dice.map((die, i) => {
@@ -55,9 +62,9 @@ export default function Page() {
                     })}
                 </div>
 
-                <Button className={'cursor-pointer w-[100] h-[60] text-xl font-semibold'}
+                <Button className={'cursor-pointer w-[150] h-[60] text-xl font-semibold'}
                 onClick={rollDice}>
-                    Roll
+                    {gameWon ? `New Game` : `Roll`}
                 </Button>
             </div>
         </main>
